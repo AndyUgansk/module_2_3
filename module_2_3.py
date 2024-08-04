09a9841248cbbdfc8660255b4b15b8b07116758d
AndyUgansk <andyugansk@gmail.com>
2024-08-04 18:23:55 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 n_list = len(my_list)
 n = 0
+# Начинаем цикл до первого отрицательного значения либо до конца списка 'my_list'
 while n <= n_list and my_list[n] >= 0:
     if my_list[n] == 0:
         n += 1
@@ -8,3 +9,4 @@ while n <= n_list and my_list[n] >= 0:
     else:
         print(my_list[n])
     n += 1
+#  End of file
